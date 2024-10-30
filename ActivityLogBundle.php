3a7173b2e53bb45420e103251207b58ec0c1b98d
marc <marc.vanduivenvoorde@endeavour.nl>
2024-10-30 13:08:27 +0100

    - add void to build method

--- ActivityLogBundle.php
+++ ActivityLogBundle.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
  */
 class ActivityLogBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function build(ContainerBuilder $container): void
     {
         $container->addCompilerPass(new FormatterPass());
     }
